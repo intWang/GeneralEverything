e64a05c30785a2c6051b10e3cf2666b529682e91
Ace Wang <ace.wang@ringcentral.com>
2026-05-03 23:44:22 +0800

    fix: include api package in installs

--- apps/api/setup.py
+++ apps/api/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 
-setup(name="get-api", version="0.1.0")
+setup(name="get-api", version="0.1.0", packages=find_packages())
